116a0ec3c6e04613d7c9ca228135a23043c7acfe
MateraBradley-FS-1 <bfmatera@student.fullsail.edu>
2024-12-06 20:06:32 -0600

    Enhance Home and Dashboard components with new styles and animations; add welcome title styling and improve layout

--- reactjs/src/pages/Home.js
+++ reactjs/src/pages/Home.js
@@ -4,9 +4,8 @@ import '../styles/App.css';
 function Home() {
   return (
     <div className="App animated-background">
-      {/* Add animated background class */}
       <header className="App-header">
-        <h1>Welcome to the Student List</h1>
+        <h1 className="welcome-title">Welcome to the Student List</h1>
         <Link to="/dashboard" className="dashboard-button">
           Dashboard
         </Link>
